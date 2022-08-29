367b8565351bfcf59acce8337e957ce989137a22
Gupil Kodali <gkodali2@huskers.unl.edu>
2022-08-29 18:38:13 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Gupil Kodali
+ *         gkodali2@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Gupil Kodali\n");
+
+  return 0;
+}
